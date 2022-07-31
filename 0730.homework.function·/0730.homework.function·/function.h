a84887e252fe9ccb454cbad75885ffefd1933f41
cct010 <107844670+cct010@users.noreply.github.com>
2022-07-31 08:29:23 +0800

    Create function.h

---
+++ 0730.homework.function·/0730.homework.function·/function.h
@@ -0,0 +1,15 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<math.h>
+int prime(int n);
+void swap(int a, int b);
+void Mult_table(int n);
+int leap_year(int n);
+
+
+#endif // !__FUNCTION_H__
+
